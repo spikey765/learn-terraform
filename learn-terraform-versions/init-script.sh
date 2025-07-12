12e73cd2241173c36013ac0396acb0efae9f1e56
Taimor Alam <alamtaimor@gmail.com>
2025-07-12 21:00:31 +0100

    Learnt resources & versioning

---
+++ learn-terraform-versions/init-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+yum update -y
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+yum install -y httpd mariadb-server
+systemctl start httpd
+systemctl enable httpd
+
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+cd /var/www/html
+curl http://169.254.169.254/latest/meta-data/instance-id -o index.html
+curl https://raw.githubusercontent.com/hashicorp/learn-terramino/master/index.php -O
---
+++ learn-terraform-versions/terraform.tf
@@ -0,0 +1,15 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 5.52.0"
+    }
+    random = {
+      version = "~> 3.6.2"
+    }
+  }
+
+  required_version = "~> 1.12.2"
+}
