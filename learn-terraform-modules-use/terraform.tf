589284739cf74eb87422cd0ff50014ff47ad8edb
Taimor Alam <alamtaimor@gmail.com>
2025-07-15 20:22:31 +0100

    First modules tutorial

---
+++ learn-terraform-modules-use/terraform.tf
@@ -0,0 +1,21 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  
+  cloud {
+    organization = "devops-practicing"
+    workspaces {
+      name = "learn-terraform-module-use"
+    }
+  }
+  
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.49.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
